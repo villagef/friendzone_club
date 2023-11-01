9d700034e5917dcaa3b548a17a2bc7c5ee54341d
villagef <filwyd123@gmail.com>
2023-11-01 19:22:25 +0100

    feat: add footer and modify content

---
+++ config/footer.ts
@@ -0,0 +1,18 @@
+export const footerConfig = {
+  support: {
+    name: "Help & Support",
+    href: "/support",
+  },
+  terms: {
+    name: "Terms",
+    href: "/terms",
+  },
+  privacy: {
+    name: "Privacy",
+    href: "/privacy",
+  },
+  cookies: {
+    name: "Cookie Policy",
+    href: "/cookies",
+  },
+};
