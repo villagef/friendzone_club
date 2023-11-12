5c6af46daccda7dd02b2261174b6bc2df334bd97
villagef <filwyd123@gmail.com>
2023-11-12 21:13:32 +0100

    refactor: signin form types

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,47 +5,33 @@ import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { signIn, useSession } from "next-auth/react"
-import { FieldValues, FormProvider, useForm } from "react-hook-form"
+import { FieldValues, useForm } from "react-hook-form"
 import { toast } from "sonner"
-import { object, string } from "zod"
 
+import { signinSchema, SignInSchemaType } from "@/lib/types"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Authentication from "@/components/Authentication"
 import { Icons } from "@/components/icons"
 
-const loginSchema = object({
-  email: string()
-    .email({ message: "The email is invalid." })
-    .min(1, { message: "required" })
-    .default(""),
-  password: string().min(1, {
-    message: "required",
-  }),
-})
-
 export default function SignInForm() {
   const { data: session } = useSession()
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
-  const methods = useForm({
-    resolver: zodResolver(loginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInSchemaType>({
+    resolver: zodResolver(signinSchema),
   })
   const route = useRouter()
   const handleSignInWithGoogle = (event: React.SyntheticEvent) => {
     event.preventDefault()
-    setIsLoading(true)
-    signIn("google").finally(() => {
-      setIsLoading(false)
-    })
+    signIn("google")
   }
 
   function onSubmit(data: FieldValues) {
-    setIsLoading(true)
     signIn("credentials", {
       ...data,
       redirect: false,
@@ -62,12 +48,9 @@ export default function SignInForm() {
         console.error(err)
         toast.error("Something went wrong", { position: "top-right" })
       })
-      .finally(() => {
-        setIsLoading(false)
-      })
   }
 
-  const allFieldsFilled = Object.values(methods.watch()).every(Boolean)
+  const allFieldsFilled = Object.values(watch()).every(Boolean)
 
   return (
     <Authentication isSignup={!!session}>
@@ -79,68 +62,60 @@ export default function SignInForm() {
           <p className="text-sm">Enter your credentails below to sign in</p>
         </div>
         <div className={"grid gap-6"}>
-          <FormProvider {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit)} noValidate>
-              <div className="grid gap-2">
-                <div className="grid gap-1">
-                  <Label className="" htmlFor="email">
-                    Email{" "}
-                    {methods.formState.errors.email && (
-                      <span className="ml-1 text-xs text-destructive">
-                        {
-                          methods.formState.errors.email
-                            .message as React.ReactNode
-                        }
-                      </span>
-                    )}
-                  </Label>
-                  <Input
-                    id="email"
-                    {...methods.register("email")}
-                    placeholder="name@example.com"
-                    type="email"
-                    autoCapitalize="none"
-                    autoComplete="email"
-                    autoCorrect="off"
-                    disabled={isLoading}
-                    className="text-primary"
-                  />
-                </div>
-                <Label className="" htmlFor="password">
-                  Password{" "}
-                  {methods.formState.errors.password && (
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <div className="grid gap-2">
+              <div className="grid gap-1">
+                <Label className="" htmlFor="email">
+                  Email{" "}
+                  {errors.email && (
                     <span className="ml-1 text-xs text-destructive">
-                      {
-                        methods.formState.errors.password
-                          .message as React.ReactNode
-                      }
+                      {errors.email.message as React.ReactNode}
                     </span>
                   )}
                 </Label>
                 <Input
-                  id="password"
-                  {...methods.register("password")}
-                  placeholder="password"
-                  type="password"
+                  id="email"
+                  {...register("email")}
+                  placeholder="name@example.com"
+                  type="email"
                   autoCapitalize="none"
-                  autoComplete="password"
+                  autoComplete="email"
                   autoCorrect="off"
-                  disabled={isLoading}
+                  disabled={isSubmitting}
                   className="text-primary"
                 />
-                <Button
-                  variant={"primary"}
-                  disabled={!allFieldsFilled || isLoading}
-                  className="mt-4"
-                >
-                  {isLoading && (
-                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                  )}
-                  Sign in
-                </Button>
               </div>
-            </form>
-          </FormProvider>
+              <Label className="" htmlFor="password">
+                Password{" "}
+                {errors.password && (
+                  <span className="ml-1 text-xs text-destructive">
+                    {errors.password.message as React.ReactNode}
+                  </span>
+                )}
+              </Label>
+              <Input
+                id="password"
+                {...register("password")}
+                placeholder="password"
+                type="password"
+                autoCapitalize="none"
+                autoComplete="password"
+                autoCorrect="off"
+                disabled={isSubmitting}
+                className="text-primary"
+              />
+              <Button
+                variant={"primary"}
+                disabled={!allFieldsFilled || isSubmitting}
+                className="mt-4"
+              >
+                {isSubmitting && (
+                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                )}
+                Sign in
+              </Button>
+            </div>
+          </form>
           <div className="text-center text-xs">
             Not a member?
             <Link href="/signup">
@@ -162,9 +137,9 @@ export default function SignInForm() {
               onClick={handleSignInWithGoogle}
               variant="outline"
               type="button"
-              disabled={isLoading}
+              disabled={isSubmitting}
             >
-              {isLoading ? (
+              {isSubmitting ? (
                 <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
               ) : (
                 <Icons.google className="mr-2" />
--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,6 @@
 import { z } from "zod"
 
+//SIGNUP SCHEMA
 export const signupSchema = z.object({
   gender: z.string().min(1, { message: "required" }).default(""),
   location: z.string().min(1, { message: "required" }).default(""),
@@ -21,3 +22,19 @@ export const signupSchema = z.object({
 export type SignUpSchemaType = z.infer<typeof signupSchema>
 
 export type SignupSchemaKeys = keyof SignUpSchemaType
+
+//SIGNIN SCHEMA
+export const signinSchema = z.object({
+  email: z
+    .string()
+    .email({ message: "The email is invalid." })
+    .min(1, { message: "required" })
+    .default(""),
+  password: z.string().min(1, {
+    message: "required",
+  }),
+})
+
+export type SignInSchemaType = z.infer<typeof signinSchema>
+
+export type SignInSchemaKeys = keyof SignInSchemaType
